f2778dac21a8bb93ce16c9ed49ca0ad862fd2d5d
Hknots <hknots99@gmail.com>
2025-01-14 13:21:51 +0100

    create FintProperties

---
+++ src/main/kotlin/no/fintlabs/consumer/deployer/config/FintProperties.kt
@@ -0,0 +1,8 @@
+package no.fintlabs.consumer.deployer.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "fint")
+data class FintProperties(
+    val consumerUrl: String
+)
