786b1f7f3ccf183f4f8ff382d4e6e472459c9830
Hknots <hknots99@gmail.com>
2025-01-27 13:34:40 +0100

    fix: use models from state library

--- src/main/kotlin/no/fintlabs/consumer/deployer/DeployerService.kt
+++ src/main/kotlin/no/fintlabs/consumer/deployer/DeployerService.kt
@@ -2,42 +2,42 @@ package no.fintlabs.consumer.deployer
 
 import com.sun.org.slf4j.internal.Logger
 import com.sun.org.slf4j.internal.LoggerFactory
-import no.fintlabs.consumer.deployer.model.Consumer
-import no.fintlabs.consumer.deployer.model.ConsumerRequest
-import no.fintlabs.consumer.deployer.model.Operation
+import no.fintlabs.consumer.state.interfaces.Consumer
+import no.fintlabs.consumer.state.model.ConsumerResponse
+import no.fintlabs.consumer.state.model.Operation
 import org.springframework.stereotype.Service
 
 @Service
-class DeployerService(
-    private val kubectl: KubectlService
-) {
+class DeployerService(private val kubectl: KubectlService) {
 
     private val log: Logger = LoggerFactory.getLogger(DeployerService::class.java)
 
-    fun handleEvent(consumerRequest: ConsumerRequest) {
-        when (consumerRequest.operation) {
-            Operation.CREATE -> handleCreate(consumerRequest.consumer)
-            Operation.UPDATE -> handleUpdate(consumerRequest.consumer)
-            Operation.DELETE -> handleDelete(consumerRequest.consumer)
+    fun handleEvent(consumerResponse: ConsumerResponse) {
+        when (consumerResponse.operation) {
+            Operation.CREATE -> handleCreate(consumerResponse.consumer)
+            Operation.UPDATE -> handleUpdate(consumerResponse.consumer)
+            Operation.DELETE -> handleDelete(consumerResponse.consumer)
         }
     }
 
     private fun handleCreate(consumer: Consumer) {
         kubectl.takeIf { it.deploymentDoesntExist(consumer) }
             ?.create(consumer)
-            ?: log.error("Skipping creation... Deployment already exists: {}", consumer.id)
+            ?: log.error("Skipping creation... Deployment already exists: {}", createConsumerId(consumer))
     }
 
     private fun handleUpdate(consumer: Consumer) {
         kubectl.takeIf { it.deploymentExists(consumer) }
             ?.update(consumer)
-            ?: log.error("Skipping update... Deployment doesn't exist: {}", consumer.id)
+            ?: log.error("Skipping update... Deployment doesn't exist: {}", createConsumerId(consumer))
     }
 
     private fun handleDelete(consumer: Consumer) {
         kubectl.takeIf { it.deploymentExists(consumer) }
             ?.delete(consumer)
-            ?: log.error("Skipping deletion... Deployment doesn't exist: {} ", consumer.id)
+            ?: log.error("Skipping deletion... Deployment doesn't exist: {} ", createConsumerId(consumer))
     }
+
+    private fun createConsumerId(consumer: Consumer) = "${consumer.domain}-${consumer.`package`}-${consumer.org}"
 
 }
