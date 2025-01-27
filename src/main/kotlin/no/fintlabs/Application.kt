cae90f07963c19a4cbebf638f31df2e7ccf45b0f
Hknots <hknots99@gmail.com>
2025-01-27 13:29:44 +0100

    annotate App with WebHookClient

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -1,9 +1,11 @@
 package no.fintlabs
 
+import no.fintlabs.webhook.client.annotation.WebHookClient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@WebHookClient
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class FintCoreConsumerDeployerApplication
