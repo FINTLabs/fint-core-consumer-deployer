7a5f3851e3eb2f58f684af050411bf348c18a061
Hknots <hknots99@gmail.com>
2025-01-28 13:11:18 +0100

    feat: add KubernetesConfig

---
+++ src/main/kotlin/no/fintlabs/consumer/deployer/config/KubernetesConfig.kt
@@ -0,0 +1,22 @@
+package no.fintlabs.consumer.deployer.config
+
+import io.fabric8.kubernetes.client.KubernetesClient
+import io.fabric8.kubernetes.client.KubernetesClientBuilder
+import io.fabric8.kubernetes.client.dsl.MixedOperation
+import io.fabric8.kubernetes.client.dsl.Resource
+import no.fintlabs.consumer.deployer.flais.model.Application
+import no.fintlabs.consumer.deployer.flais.model.ApplicationList
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class KubernetesConfig {
+
+    @Bean
+    fun kubernetesClient(): KubernetesClient = KubernetesClientBuilder().build()
+
+    @Bean
+    fun applicationClient(kubernetesClient: KubernetesClient): MixedOperation<Application, ApplicationList, Resource<Application>> =
+        kubernetesClient.resources(Application::class.java, ApplicationList::class.java)
+
+}
