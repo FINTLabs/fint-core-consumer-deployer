2d4d01932147c9246bb33371d561551d5fa57388
Hknots <hknots99@gmail.com>
2025-01-28 13:20:53 +0100

    feat: fill deploymentExists function

--- src/main/kotlin/no/fintlabs/consumer/deployer/KubectlService.kt
+++ src/main/kotlin/no/fintlabs/consumer/deployer/KubectlService.kt
@@ -1,12 +1,24 @@
 package no.fintlabs.consumer.deployer
 
-import no.fintlabs.consumer.deployer.model.Consumer
+import io.fabric8.kubernetes.client.dsl.MixedOperation
+import io.fabric8.kubernetes.client.dsl.Resource
+import no.fintlabs.consumer.deployer.config.FintProperties
+import no.fintlabs.consumer.deployer.flais.model.Application
+import no.fintlabs.consumer.deployer.flais.model.ApplicationList
+import no.fintlabs.consumer.state.interfaces.Consumer
 import org.springframework.stereotype.Service
 
 @Service
-class KubectlService {
+class KubectlService(
+    private val applicationClient: MixedOperation<Application, ApplicationList, Resource<Application>>,
+    private val fintProperties: FintProperties
+) {
 
-    fun deploymentExists(consumer: Consumer): Boolean = TODO()
+    fun deploymentExists(consumer: Consumer): Boolean =
+        applicationClient
+            .inNamespace(formatNameSpace(consumer.org))
+            .withName(createDeploymentName(consumer.domain, consumer.`package`))
+            .get() != null
 
     fun deploymentDoesntExist(consumer: Consumer) = !deploymentExists(consumer)
 
@@ -15,5 +27,8 @@ class KubectlService {
     fun update(consumer: Consumer) = TODO()
 
     fun delete(consumer: Consumer) = TODO()
+
+    fun createDeploymentName(domain: String, `package`: String) = "fint-core-consumer-$domain-$`package`"
+    fun formatNameSpace(org: String) = org.replace(".", "-")
 
 }
