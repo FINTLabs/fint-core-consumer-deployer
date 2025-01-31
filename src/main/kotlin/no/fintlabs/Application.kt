8ab5c80f48c55d4d1e08164cd4859e7f514a8018
Hknots <hknots99@gmail.com>
2025-01-31 10:07:49 +0100

    chore: disable webhook

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -1,15 +1,13 @@
 package no.fintlabs
 
-import no.fintlabs.webhook.client.annotation.WebHookClient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@WebHookClient
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class FintCoreConsumerDeployerApplication
 
 fun main(args: Array<String>) {
-	runApplication<FintCoreConsumerDeployerApplication>(*args)
+    runApplication<FintCoreConsumerDeployerApplication>(*args)
 }
