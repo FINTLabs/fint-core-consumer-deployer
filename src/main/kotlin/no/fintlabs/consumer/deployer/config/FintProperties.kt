b73bc0eac0a3acda3c060a10f8ccc2003c889c95
Hknots <hknots99@gmail.com>
2025-02-03 13:20:21 +0100

    chore: remove unused field

--- src/main/kotlin/no/fintlabs/consumer/deployer/config/FintProperties.kt
+++ src/main/kotlin/no/fintlabs/consumer/deployer/config/FintProperties.kt
@@ -4,6 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "fint")
 data class FintProperties(
-    val consumerUrl: String,
     val env: String
 )
