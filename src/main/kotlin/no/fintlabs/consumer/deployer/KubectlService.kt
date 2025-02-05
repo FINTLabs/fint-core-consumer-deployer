b373eeb37f82be1e6e6da4d12d74e0dfc19046c8
Hknots <hknots99@gmail.com>
2025-02-05 13:16:32 +0100

    feat: add support for creating namespace & update

--- src/main/kotlin/no/fintlabs/consumer/deployer/KubectlService.kt
+++ src/main/kotlin/no/fintlabs/consumer/deployer/KubectlService.kt
@@ -1,6 +1,8 @@
 package no.fintlabs.consumer.deployer
 
+import io.fabric8.kubernetes.api.model.NamespaceBuilder
 import io.fabric8.kubernetes.api.model.StatusDetails
+import io.fabric8.kubernetes.client.KubernetesClient
 import io.fabric8.kubernetes.client.dsl.MixedOperation
 import io.fabric8.kubernetes.client.dsl.Resource
 import no.fintlabs.consumer.deployer.config.FintProperties
@@ -13,16 +15,23 @@ import org.springframework.stereotype.Service
 @Service
 class KubectlService(
     private val applicationClient: MixedOperation<Application, ApplicationList, Resource<Application>>,
+    private val kubernetesClient: KubernetesClient,
     private val fintProperties: FintProperties
 ) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    fun deploymentExists(consumer: Consumer): Boolean =
-        applicationClient
-            .inNamespace(formatNameSpace(consumer.org))
+    fun deploymentExists(consumer: Consumer): Boolean {
+        val namespace = formatNameSpace(consumer.org)
+
+        ensureNamespaceExists(namespace)
+
+        return applicationClient
+            .inNamespace(namespace)
             .withName(createDeploymentName(consumer.domain, consumer.`package`))
             .get() != null
+    }
+
 
     fun deploymentDoesntExist(consumer: Consumer) = !deploymentExists(consumer)
 
@@ -37,7 +46,49 @@ class KubectlService(
         return application
     }
 
-    fun update(consumer: Consumer): Boolean = false
+    fun ensureNamespaceExists(namespace: String) {
+        val existingNamespace = kubernetesClient.namespaces().withName(namespace).get()
+
+        if (existingNamespace == null) {
+            logger.info("Namespace $namespace does not exist. Creating...")
+
+            val namespaceResource = NamespaceBuilder()
+                .withNewMetadata()
+                .withName(namespace)
+                .endMetadata()
+                .build()
+
+            kubernetesClient.namespaces().resource(namespaceResource).create()
+            logger.info("Namespace $namespace created successfully.")
+        } else {
+            logger.debug("Namespace $namespace already exists.")
+        }
+    }
+
+    fun getExistingApplication(namespace: String, deploymentName: String): Application =
+        applicationClient.inNamespace(namespace)
+            .withName(deploymentName)
+            .get()
+
+    fun update(consumer: Consumer): Application? {
+        val namespace = formatNameSpace(consumer.org)
+        val deploymentName = createDeploymentName(consumer.domain, consumer.`package`)
+        val existingApplication = getExistingApplication(namespace, deploymentName)
+
+        val updatedApplication = existingApplication.apply {
+            spec = Application.fromConsumer(consumer, fintProperties.env).spec
+        }
+
+        val appliedApplication = applicationClient
+            .inNamespace(namespace)
+            .resource(updatedApplication)
+            .update()
+
+        logger.info("Successfully updated application: ${appliedApplication.metadata.name} in namespace: $namespace")
+
+        return appliedApplication
+    }
+
 
     fun delete(consumer: Consumer): MutableList<StatusDetails> {
         val statusDetails = applicationClient
