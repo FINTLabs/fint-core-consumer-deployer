cc8f230b7111469d6a3f8aa6bdf014cd8da9ce82
Hknots <hknots99@gmail.com>
2025-02-06 00:07:34 +0100

    test: setup basic KubectlService

---
+++ src/test/kotlin/no/fintlabs/consumer/deployer/KubectlServiceTest.kt
@@ -0,0 +1,51 @@
+package no.fintlabs.consumer.deployer
+
+import io.fabric8.kubernetes.client.KubernetesClient
+import no.fintlabs.consumer.deployer.config.TestKubernetesConfig
+import no.fintlabs.consumer.state.interfaces.Consumer
+import no.fintlabs.consumer.state.model.ConsumerRequest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(properties = ["spring.main.allow-bean-definition-overriding=true"])
+@Import(TestKubernetesConfig::class)
+@ActiveProfiles("test")
+class KubectlServiceTest {
+
+    @Autowired
+    lateinit var kubectlService: KubectlService
+
+    @Autowired
+    lateinit var kubernetesClient: KubernetesClient
+
+    private val testOrg = "fintlabs.no"
+    private val testConsumer = ConsumerRequest("utdanning", "vurdering", "3.19.3", testOrg)
+
+    fun expectedDeploymentName(consumer: Consumer): String =
+        "fint-core-consumer-${consumer.domain}-${consumer.`package`}"
+
+    @BeforeEach
+    fun setup() {
+        try {
+            kubernetesClient.namespaces().withName(testOrg).delete()
+        } catch (_: Exception) {
+        }
+    }
+
+    @Test
+    fun `test ensureNamespaceExists creates a namespace if missing`() {
+        val namespace = kubectlService.formatNamespace(testOrg)
+        val nsBefore = kubernetesClient.namespaces().withName(namespace).get()
+        assertNull(nsBefore)
+
+        kubectlService.ensureNamespaceExists(testOrg)
+        val nsAfter = kubernetesClient.namespaces().withName(namespace).get()
+        assertNotNull(nsAfter)
+    }
+
+}
