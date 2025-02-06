0d28f6c65303fb6e9fa2c1b0fc39a3d95308a3b3
Hknots <hknots99@gmail.com>
2025-02-06 19:02:55 +0100

    test: remove properties in anootation

--- src/test/kotlin/no/fintlabs/consumer/deployer/KubectlServiceTest.kt
+++ src/test/kotlin/no/fintlabs/consumer/deployer/KubectlServiceTest.kt
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest(properties = ["spring.main.allow-bean-definition-overriding=true"])
+@SpringBootTest
 @Import(TestKubernetesConfig::class)
 @ActiveProfiles("test")
 class KubectlServiceTest {
