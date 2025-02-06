eb551488760eef85099f2ed4214a72cbfcdc9271
Hknots <hknots99@gmail.com>
2025-02-06 22:37:19 +0100

    test: setup new tools & tests

--- src/test/kotlin/no/fintlabs/consumer/deployer/KubectlServiceTest.kt
+++ src/test/kotlin/no/fintlabs/consumer/deployer/KubectlServiceTest.kt
@@ -1,17 +1,22 @@
 package no.fintlabs.consumer.deployer
 
 import io.fabric8.kubernetes.client.KubernetesClient
+import io.fabric8.kubernetes.client.dsl.MixedOperation
+import io.fabric8.kubernetes.client.dsl.Resource
 import no.fintlabs.consumer.deployer.config.TestKubernetesConfig
-import no.fintlabs.consumer.state.interfaces.Consumer
+import no.fintlabs.consumer.deployer.flais.model.Application
+import no.fintlabs.consumer.deployer.flais.model.ApplicationList
 import no.fintlabs.consumer.state.model.ConsumerRequest
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
-import kotlin.math.exp
+import kotlin.random.Random
 
 @SpringBootTest
 @Import(TestKubernetesConfig::class)
@@ -24,71 +29,95 @@ class KubectlServiceTest {
     @Autowired
     lateinit var kubernetesClient: KubernetesClient
 
+    @Autowired
+    lateinit var applicationClient: MixedOperation<Application, ApplicationList, Resource<Application>>
+
     private val testOrg = "fintlabs.no"
     private val testConsumer = ConsumerRequest("utdanning", "vurdering", "3.19.3", testOrg)
-
-    fun expectedDeploymentName(consumer: Consumer): String =
-        "fint-core-consumer-${consumer.domain}-${consumer.`package`}"
+    private val logger = LoggerFactory.getLogger(this::class.java)
+    private var generatedCount: Int = 0
 
     @BeforeEach
     fun setup() {
-        try {
-            kubernetesClient.namespaces().withName(testOrg).delete()
-        } catch (_: Exception) {
-        }
+        generatedCount = Random.nextInt(10, 100)
     }
 
-    // TODO: Test update among many, verify integrity of each application
+    @AfterEach
+    fun teardown() {
+        applicationClient
+            .inAnyNamespace()
+            .list()
+            .items
+            .forEach { app ->
+                applicationClient
+                    .inNamespace(app.metadata.namespace)
+                    .withName(app.metadata.name)
+                    .delete()
+            }
 
-    // TODO: Test creation when Application already exists
-
-    // TODO: Test creation among many resources, verify it exists
-
-    @Test
-    fun `test deletion doesnt delete anything when no Application match`() {
-        val expectedAmount = 100
-        generateRandomApplications(expectedAmount)
-        assertEquals(expectedAmount, kubectlService.applicationCount())
-        assertEquals(0, kubectlService.delete(testConsumer).size)
+        kubernetesClient.namespaces()
+            .list()
+            .items
+            .forEach { ns ->
+                kubernetesClient.namespaces()
+                    .withName(ns.metadata.name)
+                    .delete()
+            }
     }
 
     @Test
-    fun `test deletion of existing Application`() {
-        val expectedAmount = 100
-        generateRandomApplications(expectedAmount)
+    fun `test ensure namespace`() {
+        val originalNamespaceCount = namespaceCount()
+
+        kubectlService.ensureNamespace(testOrg)
+
+        val expectedNamespace = kubectlService.formatNamespace(testOrg)
+
+        assertTrue(
+            namespaceCount() >= originalNamespaceCount + 1,
+            "Expected at least one new namespace, but none appeared"
+        )
+
+        val namespaceResource = kubernetesClient.namespaces().withName(expectedNamespace).get()
+        assertNotNull(namespaceResource, "Namespace $expectedNamespace was not found after ensureNamespace()")
+    }
+
+    @Test
+    fun `test delete Application verbose`() =
+        triggerVerboseTesting({ `test delete Application`() }, 0)
+
+    @Test
+    fun `test delete Application`() {
+        val createApplication = kubectlService.createApplication(testConsumer)
+        val delete = kubectlService.delete(testConsumer)
+        assertEquals(createApplication.metadata.name, delete.first().name)
+        assertTrue(delete.size == 1)
+    }
+
+    @Test
+    fun `test update Application verbose`() =
+        triggerVerboseTesting({ `test update Application`() }, 1)
+
+    @Test
+    fun `test update Application`() {
         kubectlService.createApplication(testConsumer)
-
-        assertEquals(expectedAmount + 1, kubectlService.applicationCount())
-        assertNotNull(kubectlService.getApplication(testConsumer))
-
-        kubectlService.delete(testConsumer)
-
-        assertNull(kubectlService.getApplication(testConsumer))
-        assertEquals(expectedAmount, kubectlService.applicationCount())
-    }
-
-    @Test
-    fun `test get existing Application returns null`() =
-        assertNull(kubectlService.getApplication(testConsumer))
-
-    @Test
-    fun `test get existing Application`() {
-        val createdApplication = kubectlService.createApplication(testConsumer)
         val existingApplication = kubectlService.getApplication(testConsumer)
 
-        assertEquals(createdApplication.metadata.name, existingApplication!!.metadata.name)
+        kubectlService.updateApplication(testConsumer.copy(version = "3.20.5"))
+
+        val updatedApplication = kubectlService.getApplication(testConsumer)
+
+        assertNotEquals(existingApplication!!.spec.image, updatedApplication!!.spec.image)
     }
 
     @Test
-    fun `test ensureNamespaceExists creates a namespace if missing`() {
-        val namespace = kubectlService.formatNamespace(testOrg)
-        val nsBefore = kubernetesClient.namespaces().withName(namespace).get()
-        assertNull(nsBefore)
+    fun `test create Application verbose`() =
+        triggerVerboseTesting({ `test create Application`() }, 1)
 
-        kubectlService.ensureNamespaceExists(testOrg)
-
-        val nsAfter = kubernetesClient.namespaces().withName(namespace).get()
-        assertNotNull(nsAfter)
+    @Test
+    fun `test create Application`() {
+        val createApplication = kubectlService.createApplication(testConsumer)
+        assertNotNull(createApplication)
     }
 
     @Test
@@ -105,15 +134,43 @@ class KubectlServiceTest {
             kubectlService.formatNamespace(testOrg)
         )
 
-    fun generateRandomApplications(amount: Int) {
+    fun applications(): ApplicationList = applicationClient.inAnyNamespace().list()
+
+    fun applicationCount(): Int = applicationClient.inAnyNamespace().list().items.size
+
+    fun namespaceCount(): Int = kubernetesClient.namespaces().list().items.size
+
+    fun triggerVerboseTesting(action: () -> Unit, unitApplicationCount: Int) {
+        val generateRandomApplications = generateRandomApplications(generatedCount)
+
+        action()
+
+        assertEquals(generatedCount + unitApplicationCount, applicationCount())
+        verifyApplications(generateRandomApplications)
+    }
+
+    fun generateRandomApplications(amount: Int): List<Application> {
+        val createdApplications: MutableList<Application> = mutableListOf()
         for (i in 1..amount) {
-            val consumer = ConsumerRequest(
-                domain = "domain$i",
-                `package` = "package$i",
-                version = "1.0.$i",
-                org = "$testOrg-$i"
-            )
-            kubectlService.createApplication(consumer)
+            val application = kubectlService.createApplication(TestTools.randomConsumerRequest())
+            createdApplications.add(application)
+        }
+        return createdApplications
+    }
+
+    fun verifyApplications(createdApplications: List<Application>) {
+        val clusterApplications = applications().items
+
+        logger.info("Verifying ${createdApplications.size} applications")
+        createdApplications.forEach { expected ->
+            val found = clusterApplications.find { actual ->
+                actual.metadata.name == expected.metadata.name &&
+                        actual.metadata.namespace == expected.metadata.namespace
+            }
+
+            assertNotNull(found, "Could not find Application '${expected.metadata.name}' in the cluster.")
+
+            found?.let { actual -> TestTools.verifySpec(expected.spec, actual.spec) }
         }
     }
 
---
+++ src/test/kotlin/no/fintlabs/consumer/deployer/TestTools.kt
@@ -0,0 +1,104 @@
+package no.fintlabs.consumer.deployer
+
+import no.fintlabs.consumer.deployer.flais.model.ApplicationSpec
+import no.fintlabs.consumer.state.model.ConsumerRequest
+import no.fintlabs.consumer.state.model.Limits
+import no.fintlabs.consumer.state.model.PodResources
+import no.fintlabs.consumer.state.model.Requests
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.util.UUID
+import kotlin.random.Random
+
+class TestTools {
+    companion object {
+
+        fun randomConsumerRequest(): ConsumerRequest {
+            fun randomString() = UUID.randomUUID().toString()
+            fun randomBoolean() = Random.nextBoolean()
+
+            return ConsumerRequest(
+                domain = "domain-${randomString()}",
+                `package` = "package-${randomString()}",
+                version = "version-${randomString()}",
+                org = "org-${randomString()}",
+                shared = randomBoolean(),
+                podResources = PodResources(
+                    limits = Limits(
+                        memory = "mem-limit-${randomString()}",
+                        cpu = "cpu-limit-${randomString()}"
+                    ),
+                    requests = Requests(
+                        memory = "mem-req-${randomString()}",
+                        cpu = "cpu-req-${randomString()}"
+                    )
+                ),
+                resources = listOf("res1-${randomString()}", "res2-${randomString()}"),
+                writeableResources = listOf("wres1-${randomString()}"),
+                cacheDisabledResources = listOf("cdres1-${randomString()}")
+            )
+        }
+
+        fun verifySpec(expected: ApplicationSpec, actual: ApplicationSpec) {
+            assertEquals(expected.image, actual.image, "image mismatch")
+            assertEquals(expected.imagePullPolicy, actual.imagePullPolicy, "imagePullPolicy mismatch")
+            assertEquals(expected.orgId, actual.orgId, "orgId mismatch")
+            assertEquals(expected.port, actual.port, "port mismatch")
+            assertEquals(expected.replicas, actual.replicas, "replicas mismatch")
+            assertEquals(expected.restartPolicy, actual.restartPolicy, "restartPolicy mismatch")
+
+            assertEquals(expected.imagePullSecrets, actual.imagePullSecrets, "imagePullSecrets mismatch")
+
+            assertEquals(expected.database?.database, actual.database?.database, "database.database mismatch")
+            assertEquals(expected.database?.enabled, actual.database?.enabled, "database.enabled mismatch")
+
+            assertEquals(expected.env?.size, actual.env?.size, "env.size mismatch")
+            expected.env?.forEachIndexed { i, e ->
+                val a = actual.env?.get(i)
+                assertEquals(e.name,  a?.name,  "env[$i].name mismatch")
+                assertEquals(e.value, a?.value, "env[$i].value mismatch")
+            }
+
+            assertEquals(expected.envFrom?.size, actual.envFrom?.size, "envFrom.size mismatch")
+            expected.envFrom?.forEachIndexed { i, e ->
+                val a = actual.envFrom?.get(i)
+                assertEquals(e.prefix,          a?.prefix, "envFrom[$i].prefix mismatch")
+            }
+
+            assertEquals(expected.ingress?.enabled,   actual.ingress?.enabled,   "ingress.enabled mismatch")
+            assertEquals(expected.ingress?.basePath,  actual.ingress?.basePath,  "ingress.basePath mismatch")
+            assertEquals(expected.ingress?.middlewares, actual.ingress?.middlewares, "ingress.middlewares mismatch")
+            assertEquals(expected.ingress?.routes?.size, actual.ingress?.routes?.size, "routes.size mismatch")
+
+            expected.ingress?.routes?.forEachIndexed { i, r ->
+                val ar = actual.ingress?.routes?.get(i)
+                assertEquals(r.host,       ar?.host,       "routes[$i].host mismatch")
+                assertEquals(r.path,       ar?.path,       "routes[$i].path mismatch")
+                assertEquals(r.headers,    ar?.headers,    "routes[$i].headers mismatch")
+                assertEquals(r.queries,    ar?.queries,    "routes[$i].queries mismatch")
+                assertEquals(r.middlewares,ar?.middlewares,"routes[$i].middlewares mismatch")
+            }
+
+            assertEquals(expected.kafka?.enabled, actual.kafka?.enabled, "kafka.enabled mismatch")
+
+            assertEquals(expected.observability?.logging, actual.observability?.logging, "observability.logging mismatch")
+            assertEquals(expected.observability?.metrics, actual.observability?.metrics, "observability.metrics mismatch")
+
+            assertEquals(expected.onePassword?.itemPath, actual.onePassword?.itemPath, "onePassword.itemPath mismatch")
+
+            assertEquals(expected.prometheus?.enabled, actual.prometheus?.enabled, "prometheus.enabled mismatch")
+            assertEquals(expected.prometheus?.path,    actual.prometheus?.path,    "prometheus.path mismatch")
+            assertEquals(expected.prometheus?.port,    actual.prometheus?.port,    "prometheus.port mismatch")
+
+            assertEquals(expected.resources?.limits,   actual.resources?.limits,   "resources.limits mismatch")
+            assertEquals(expected.resources?.requests, actual.resources?.requests, "resources.requests mismatch")
+
+            assertEquals(expected.strategy?.type,           actual.strategy?.type,           "strategy.type mismatch")
+            assertEquals(expected.strategy?.rollingUpdate,  actual.strategy?.rollingUpdate,  "strategy.rollingUpdate mismatch")
+
+            assertEquals(expected.url?.basePath, actual.url?.basePath, "url.basePath mismatch")
+            assertEquals(expected.url?.hostname, actual.url?.hostname, "url.hostname mismatch")
+        }
+
+
+    }
+}
