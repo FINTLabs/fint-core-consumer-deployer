e711c2e1690bcedd5ac788bd676d698d99b03fb8
Hknots <hknots99@gmail.com>
2025-02-14 18:27:51 +0100

    fix: set correct annotation

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -1,9 +1,11 @@
 package no.fintlabs
 
+import no.fintlabs.webhook.client.annotation.WebhookClient
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@WebhookClient
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class FintCoreConsumerDeployerApplication
