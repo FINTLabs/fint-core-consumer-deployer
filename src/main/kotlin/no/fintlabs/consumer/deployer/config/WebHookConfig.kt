507e9a22909762c0c09ac694181be015dbc87ccd
Hknots <hknots99@gmail.com>
2025-02-14 18:28:44 +0100

    feat: bean up WebhookEventHandler of ConsumerResponse

--- src/main/kotlin/no/fintlabs/consumer/deployer/config/WebHookConfig.kt
+++ src/main/kotlin/no/fintlabs/consumer/deployer/config/WebHookConfig.kt
@@ -1,17 +1,22 @@
 package no.fintlabs.consumer.deployer.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import no.fintlabs.consumer.deployer.DeployerService
 import no.fintlabs.consumer.state.model.ConsumerResponse
-import no.fintlabs.webhook.client.WebhookClientService
+import no.fintlabs.webhook.client.config.WebhookClientProperties
+import no.fintlabs.webhook.client.handler.WebhookEventHandler
+import no.fintlabs.webhook.client.handler.createWebhookEventHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class WebHookConfig(private val deployerService: DeployerService) {
+class WebHookConfig(
+    private val webhookClientProperties: WebhookClientProperties,
+    private val deployerService: DeployerService
+) {
 
     @Bean
-    fun webHookClientService(objectMapper: ObjectMapper) =
-        WebhookClientService(ConsumerResponse::class.java, deployerService::handleEvent, objectMapper)
+    fun consumerEventHandler(): WebhookEventHandler<ConsumerResponse> =
+        createWebhookEventHandler<ConsumerResponse>(webhookClientProperties, "consumer")
+        { event -> deployerService.handleConsumer(event) }
 
 }
